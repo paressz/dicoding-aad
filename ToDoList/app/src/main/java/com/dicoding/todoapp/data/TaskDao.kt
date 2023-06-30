d1e18c84444ec689ea4b5d20777132cad8dc7f25
paress <farez0830@gmail.com>
2023-06-30 14:41:10 +0700

    TODO 2, TASKDAO

--- ToDoList/app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
+++ ToDoList/app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
@@ -2,23 +2,25 @@ package com.dicoding.todoapp.data
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import androidx.room.*
 import androidx.sqlite.db.SupportSQLiteQuery
 
 //TODO 2 : Define data access object (DAO)
+@Dao
 interface TaskDao {
-
+    @RawQuery(observedEntities = [Task::class])
     fun getTasks(query: SupportSQLiteQuery): DataSource.Factory<Int, Task>
-
+    @Query("SELECT * FROM tasks WHERE id = :taskId")
     fun getTaskById(taskId: Int): LiveData<Task>
-
+    @Query("SELECT * FROM tasks ORDER BY dueDate DESC")
     fun getNearestActiveTask(): Task
-
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertTask(task: Task): Long
-
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertAll(vararg tasks: Task)
-
+    @Delete
     suspend fun deleteTask(task: Task)
-
+    @Query("UPDATE tasks SET completed = :completed WHERE id = :taskId")
     suspend fun updateCompleted(taskId: Int, completed: Boolean)
     
 }
