dbc2982bf8f37d5a218a5a76179d6f814f4797de
paress <farez0830@gmail.com>
2023-06-30 15:30:12 +0700

    TODO 4&5, TASKREPO getTasks()

--- ToDoList/app/src/main/java/com/dicoding/todoapp/data/TaskRepository.kt
+++ ToDoList/app/src/main/java/com/dicoding/todoapp/data/TaskRepository.kt
@@ -2,7 +2,9 @@ package com.dicoding.todoapp.data
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
+import com.dicoding.todoapp.utils.FilterUtils
 import com.dicoding.todoapp.utils.TasksFilterType
 
 class TaskRepository(private val tasksDao: TaskDao) {
@@ -29,6 +31,14 @@ class TaskRepository(private val tasksDao: TaskDao) {
     //TODO 4 : Use FilterUtils.getFilteredQuery to create filterable query
     //TODO 5 : Build PagedList with configuration
     fun getTasks(filter: TasksFilterType): LiveData<PagedList<Task>> {
+        val query = FilterUtils.getFilteredQuery(filter)
+        val tasks = tasksDao.getTasks(query)
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(PLACEHOLDERS)
+            .setInitialLoadSizeHint(PAGE_SIZE)
+            .setPageSize(PAGE_SIZE)
+            .build()
+        return LivePagedListBuilder(tasks, config).build()
         throw NotImplementedError("Not yet implemented")
     }
 
