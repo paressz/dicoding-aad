e07d06c49febaee1d53375cd13b94690814c4bfd
paress <farez0830@gmail.com>
2023-06-30 16:41:36 +0700

    TODO 6&7 TaskActivity RV, adapter. TODO 8,9,10 > TaskAdapter init viewhoder, bind data, title status

--- ToDoList/app/src/main/java/com/dicoding/todoapp/data/Task.kt
+++ ToDoList/app/src/main/java/com/dicoding/todoapp/data/Task.kt
@@ -5,17 +5,22 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 //TODO 1 : Define a local database table using the schema in app/schema/tasks.json
-@Entity
+@Entity(tableName = "tasks")
 data class Task(
+
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     val id: Int = 0,
+
     @ColumnInfo(name = "title")
     val title: String,
+
     @ColumnInfo(name = "description")
     val description: String,
+
     @ColumnInfo(name = "dueDate")
     val dueDateMillis: Long,
+
     @ColumnInfo(name = "completed")
     val isCompleted: Boolean = false
 )
---
+++ ToDoList/app/src/main/java/com/dicoding/todoapp/ui/add/AddTaskViewModel.kt
@@ -0,0 +1,4 @@
+package com.dicoding.todoapp.ui.add
+
+class AddTaskViewModel {
+}
