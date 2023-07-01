9cc6081a92ecefab476411c2e3536f3f6e61de2b
paress <farez0830@gmail.com>
2023-07-01 13:30:45 +0700

    fina

--- ToDoList/app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
+++ ToDoList/app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
@@ -3,7 +3,8 @@ package com.dicoding.todoapp.ui.list
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.dicoding.todoapp.R
 import org.junit.Rule
