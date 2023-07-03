263e0b18fade38198ae45a2bdab5a9ab6bbddf1c
paress <farez0830@gmail.com>
2023-07-03 17:12:38 +0700

    TODO 11, SettingActivity

--- HabitApp/app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
+++ HabitApp/app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
@@ -3,8 +3,11 @@ package com.dicoding.habitapp.setting
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.utils.DarkMode
+import java.util.*
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -25,6 +28,14 @@ class SettingsActivity : AppCompatActivity() {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
             //TODO 11 : Update theme based on value in ListPreference
+            val pref = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+            pref?.setOnPreferenceChangeListener { preference, newValue ->
+                updateTheme(
+                    DarkMode.valueOf(
+                        newValue.toString().uppercase(Locale.US)
+                    ).value
+                )
+            }
         }
 
         private fun updateTheme(mode: Int): Boolean {
